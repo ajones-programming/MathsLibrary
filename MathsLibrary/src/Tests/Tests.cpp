75c4be83ea5313135ebb9eda2929ef982c030db4
ajones-programming <ajones.programming@gmail.com>
2024-06-03 09:04:31 +0100

    updated tests - added timer method - simplified enum lists

--- MathsLibrary/src/Tests/Tests.cpp
+++ MathsLibrary/src/Tests/Tests.cpp
@@ -1,5 +1,12 @@
 #include "Tests.h"
 #include <iostream>
+#include <chrono>
+#include <concepts>
+#include <map>
+#include <string>
+
+template <typename T>
+T listAndGetValues(std::map<T, std::string> list, std::string testName = "");
 
 Tests::Tests()
 {
@@ -7,7 +14,7 @@ Tests::Tests()
 	//while not quit
 	bool hasQuit = false;
 	while (!hasQuit) {
-		MainOptions(hasQuit);
+		hasQuit = MainOptions();
 	}
 
 }
@@ -18,7 +25,7 @@ void Tests::ShowIntroduction()
 	
 }
 
-void Tests::MainOptions(bool &hasQuit)
+bool Tests::MainOptions()
 {
 
 	std::cout << "WHICH DO YOU WANT TO DO:" << std::endl;
@@ -28,65 +35,35 @@ void Tests::MainOptions(bool &hasQuit)
 	int value;
 	std::cin >> value;
 	if (value == 1) {
-		hasQuit = true;
+		return true;
 	}
-	else {
-		TypeCategory();
-		std::cout << std::endl;
-		std::cout << std::endl;
-		std::cout << std::endl;
-		std::cout << "========================================" << std::endl;
-		std::cout << std::endl;
-		std::cout << std::endl;
-	}
+	TypeCategory();
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "========================================" << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	return false;
 }
 
 void Tests::TypeCategory()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allMainOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
+
+	switch (listAndGetValues(allMainOptionsText,"")) {
+		using enum MAIN_TEST_TYPES;
+		case STATIC: StaticTests(); break;
+		case BASIC: BasicTests(); break;
+		case TRIG: TrigTests(); break;
+		case POWERS: PowersTests(); break;
+		case EXPONENTIALS: ExponentialsTests(); break;
+		case COLOUR: ColourTests(); break;
+		case ENCODING: EncodingTests(); break;
+		case LERPING: LerpingTests(); break;
+		case RANDOMNESS: RandomnessTests(); break;
 	}
-	MAIN_TEST_TYPES typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (MAIN_TEST_TYPES)val;
-
-	switch (typeOfTest) {
-	case MAIN_TEST_TYPES::STATIC:
-		StaticTests();
-		break;
-	case MAIN_TEST_TYPES::BASIC:
-		BasicTests();
-		break;
-	case MAIN_TEST_TYPES::TRIG:
-		TrigTests();
-		break;
-	case MAIN_TEST_TYPES::POWERS:
-		PowersTests();
-		break;
-	case MAIN_TEST_TYPES::EXPONENTIALS:
-		ExponentialsTests();
-		break;
-	case MAIN_TEST_TYPES::COLOUR:
-		ColourTests();
-		break;
-	case MAIN_TEST_TYPES::ENCODING:
-		EncodingTests();
-		break;
-	case MAIN_TEST_TYPES::LERPING:
-		LerpingTests();
-		break;
-	case MAIN_TEST_TYPES::RANDOMNESS:
-		RandomnessTests();
-		break;
-	}
-
-
-	
-
 
 }
 
@@ -94,38 +71,14 @@ void Tests::StaticTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL STATIC TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allStaticOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	STATIC_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (STATIC_TESTS)val;
 
-	switch (typeOfTest) {
-	case STATIC_TESTS::DEG_2_RAD:
-	{
-		DegToRad();
-		break;
-	}
-	case STATIC_TESTS::INFINITY__DEFINED:
-	{
-		Infinity();
-		break;
-	}	case STATIC_TESTS::NEGATIVE_INFINITY:
-	{
-		NegativeInfinity();
-		break;
-	}	case STATIC_TESTS::PI__DEFINED:
-	{
-		PI();
-		break;
-	}	case STATIC_TESTS::RAD_2_DEGREE:
-	{
-		Rad2Deg();
-		break;
-	}
+	switch (listAndGetValues(allStaticOptionsText, "STATIC")) {
+		using enum STATIC_TESTS;
+		case DEG_2_RAD: DegToRad(); break;
+		case INFINITY__DEFINED: Infinity(); break;
+		case NEGATIVE_INFINITY: NegativeInfinity(); break;
+		case PI__DEFINED: PI(); break;
+		case RAD_2_DEGREE: Rad2Deg(); break;
 	}
 }
 
@@ -133,62 +86,24 @@ void Tests::BasicTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL BASIC TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allBasicOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	BASIC_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (BASIC_TESTS)val;
 
-	switch (typeOfTest){
-	case BASIC_TESTS::ABS:
-		Abs();
-		break;
-
-	case BASIC_TESTS::APPROX:
-		Approx();
-		break;
-	case BASIC_TESTS::CIEL:
-		Ciel();
-		break;
-	case BASIC_TESTS::CIEL_TO_INT:
-		CielToInt();
-		break;
-	case BASIC_TESTS::CLAMP:
-		Clamp();
-		break;
-	case BASIC_TESTS::CLAMP01:
-		Clamp01();
-		break;
-	case BASIC_TESTS::FLOOR:
-		Floor();
-		break;
-	case BASIC_TESTS::FLOOR_TO_INT:
-		FloorToInt();
-		break;
-	case BASIC_TESTS::MAX:
-		Max();
-		break;
-	case BASIC_TESTS::MIN:
-		Min();
-		break;
-	case BASIC_TESTS::POW:
-		Pow();
-		break;
-	case BASIC_TESTS::ROUND:
-		Round();
-		break;
-	case BASIC_TESTS::ROUND_TO_INT:
-		RoundToInt();
-		break;
-	case BASIC_TESTS::SIGN:
-		Sign();
-		break;
-	case BASIC_TESTS::SQRT:
-		Sqrt();
-		break;
+	switch (listAndGetValues(allBasicOptionsText, "BASIC")) {
+		using enum BASIC_TESTS;
+		case ABS: Abs(); break;
+		case APPROX: Approx(); break;
+		case CIEL: Ciel(); break;
+		case CIEL_TO_INT: CielToInt(); break;
+		case CLAMP: Clamp(); break;
+		case CLAMP01: Clamp01(); break;
+		case FLOOR: Floor(); break;
+		case FLOOR_TO_INT: FloorToInt(); break;
+		case MAX: Max(); break;
+		case MIN: Min(); break;
+		case POW: Pow(); break;
+		case ROUND: Round(); break;
+		case ROUND_TO_INT: RoundToInt(); break;
+		case SIGN: Sign(); break;
+		case SQRT: Sqrt(); break;
 	}
 }
 
@@ -196,40 +111,17 @@ void Tests::TrigTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL TRIG TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allTrigOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	TRIG_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (TRIG_TESTS)val;
-
-	switch (typeOfTest) {
-	case TRIG_TESTS::ACOS:
-		Acos();
-		break;
-	case TRIG_TESTS::ASIN:
-		Asin();
-		break;
-	case TRIG_TESTS::ATAN:
-		Atan();
-		break;
-	case TRIG_TESTS::ATAN2:
-		Atan2();
-		break;
-	case TRIG_TESTS::COS:
-		Cos();
-		break;
-	case TRIG_TESTS::DELTAANGLE:
-		DeltaAngle();
-		break;
-	case TRIG_TESTS::SIN:
-		Sin();
-		break;
-	case TRIG_TESTS::TAN:
-		Tan();
-		break;
+	
+	switch (listAndGetValues(allTrigOptionsText, "TRIG")) {
+		using enum TRIG_TESTS;
+		case ACOS: Acos(); break;
+		case ASIN: Asin(); break;
+		case ATAN: Atan(); break;
+		case ATAN2: Atan2(); break;
+		case COS: Cos(); break;
+		case DELTAANGLE: DeltaAngle(); break;
+		case SIN: Sin(); break;
+		case TAN: Tan(); break;
 	}
 }
 
@@ -237,25 +129,12 @@ void Tests::PowersTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL POWERS TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allPowersOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	POWERS_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (POWERS_TESTS)val;
 
-	switch (typeOfTest) {
-	case POWERS_TESTS::CLOSEST_POWER_OF_TWO:
-		ClosestPowerOfTwo();
-		break;
-	case POWERS_TESTS::IS_POWER_OF_TWO:
-		IsPowerOfTwo();
-		break;
-	case POWERS_TESTS::NEXT_POWER_OF_TWO:
-		NextPowerOfTwo();
-		break;
+	switch (listAndGetValues(allPowersOptionsText, "POWERS")) {
+		using enum POWERS_TESTS;
+		case CLOSEST_POWER_OF_TWO: ClosestPowerOfTwo(); break;
+		case IS_POWER_OF_TWO: IsPowerOfTwo(); break;
+		case NEXT_POWER_OF_TWO: NextPowerOfTwo(); break;
 	}
 }
 
@@ -263,119 +142,56 @@ void Tests::ExponentialsTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL EXPONENTIALS TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allExponentialsOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	EXPONENTIALS_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (EXPONENTIALS_TESTS)val;
-
-	switch (typeOfTest) {
-	case EXPONENTIALS_TESTS::EXP:
-		Exp();
-		break;
-	case EXPONENTIALS_TESTS::LOG:
-		Log();
-		break;
-	case EXPONENTIALS_TESTS::LOG10:
-		Log10();
-		break;
+	
+	switch (listAndGetValues(allExponentialsOptionsText, "EXPONENTIAL")) {
+		using enum EXPONENTIALS_TESTS;
+		case EXP: Exp(); break;
+		case LOG: Log(); break;
+		case LOG10: Log10(); break;
 	}
 }
-
 
 void Tests::ColourTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL COLOUR TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allColourOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	COLOUR_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (COLOUR_TESTS)val;
-
-	switch (typeOfTest) {
-	case COLOUR_TESTS::GAMMA_TO_LINEAR:
-		GammaToLinear();
-		break;
-	case COLOUR_TESTS::LINEAR_TO_GAMMA:
-		LinearToGamma();
-		break;
+	
+	switch (listAndGetValues(allColourOptionsText, "COLOUR")) {
+		using enum COLOUR_TESTS;
+		case GAMMA_TO_LINEAR: GammaToLinear(); break;
+		case LINEAR_TO_GAMMA: LinearToGamma(); break;
 	}
 }
 
 void Tests::EncodingTests()
 {
-
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL ENCODING TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allEncodingOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	ENCODING_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (ENCODING_TESTS)val;
 
-	switch (typeOfTest) {
-	case ENCODING_TESTS::FLOAT_TO_HALF:
-		FloatToHalf();
-		break;
-	case ENCODING_TESTS::HALF_TO_FLOAT:
-		HalfToFloat();
-		break;
+	switch (listAndGetValues(allEncodingOptionsText, "ENCODING")) {
+		using enum ENCODING_TESTS;
+		case FLOAT_TO_HALF: FloatToHalf(); break;
+		case HALF_TO_FLOAT: HalfToFloat(); break;
 	}
 }
 
 void Tests::LerpingTests()
 {
-
-
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL LERPING TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allLerpingOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
-	}
-	LERPING_TESTS typeOfTest;
-	int val;
-	std::cin >> val;
-	typeOfTest = (LERPING_TESTS)val;
-
-	switch (typeOfTest) {
-	case (LERPING_TESTS::INVERSE_LERP):
-			InverseLerp();
-			break;
-	case (LERPING_TESTS::LERP):
-		Lerp();
-		break;
-	case (LERPING_TESTS::LERP_ANGLE):
-		LerpAngle();
-		break;
-	case (LERPING_TESTS::LERP_UNCLAMPED):
-		LerpUnclamped();
-		break;
-	case (LERPING_TESTS::MOVE_TOWARDS):
-		MoveTowards();
-		break;
-	case (LERPING_TESTS::MOVE_TOWARDS_ANGLE):
-		MoveTowardsAngle();
-		break;
-	case (LERPING_TESTS::SMOOTH_DAMP):
-		SmoothDamp();
-		break;
-	case (LERPING_TESTS::SMOOTH_DAMP_ANGLE):
-		SmoothDampAngle();
-		break;
-	case (LERPING_TESTS::SMOOTH_STEP):
-		SmoothStep();
-		break;
+	
+	switch (listAndGetValues(allLerpingOptionsText, "LERPING"))
+	{
+		using enum LERPING_TESTS;
+		case INVERSE_LERP: InverseLerp(); break;
+		case LERP: Lerp(); break;
+		case LERP_ANGLE: LerpAngle(); break;
+		case LERP_UNCLAMPED: LerpUnclamped(); break;
+		case MOVE_TOWARDS: MoveTowards(); break;
+		case MOVE_TOWARDS_ANGLE: MoveTowardsAngle(); break;
+		case SMOOTH_DAMP: SmoothDamp(); break;
+		case SMOOTH_DAMP_ANGLE: SmoothDampAngle(); break;
+		case SMOOTH_STEP: SmoothStep(); break;
 	}
 }
 
@@ -383,24 +199,35 @@ void Tests::RandomnessTests()
 {
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "ALL RANDOMNESS TESTS: " << std::endl;
-	for (std::pair<int, std::string> pair : allRandomnessOptionsText) {
-		std::cout << " - " << pair.second << ": " << pair.first << std::endl;
+
+	switch (listAndGetValues(allRandomnessOptionsText, "RANDOMNESS")) {
+		using enum RANDOMNESS_TESTS;
+		case PERLIN_NOISE: PerlinNoise(); break;
+		case PING_PONG: PingPong(); break;
+		case REPEAT: Repeat(); break;
 	}
-	RANDOMNESS_TESTS typeOfTest;
+}
+
+void Tests::StartTimer()
+{
+	_start = std::chrono::high_resolution_clock::now();
+}
+
+void Tests::StopTimer() {
+	_stop = std::chrono::high_resolution_clock::now();
+}
+
+int Tests::nanosecondsDifference() {
+	return std::chrono::duration_cast<std::chrono::nanoseconds>(_start - _stop).count();
+}
+
+template <typename T>
+T listAndGetValues(std::map<T, std::string> list, std::string testName = "") {
+	std::cout << "ALL " << testName << " TESTS:" << std::endl;
+	for (auto pair : list) {
+		std::cout << " - " << pair.second << ": " << (int)pair.first << std::endl;
+	}
 	int val;
 	std::cin >> val;
-	typeOfTest = (RANDOMNESS_TESTS)val;
-
-	switch (typeOfTest) {
-	case RANDOMNESS_TESTS::PERLIN_NOISE:
-		PerlinNoise();
-		break;
-	case RANDOMNESS_TESTS::PING_PONG:
-		PingPong();
-		break;
-	case RANDOMNESS_TESTS::REPEAT:
-		Repeat();
-		break;
-	}
+	return (T)val;
 }
--- MathsLibrary/src/Tests/Tests.h
+++ MathsLibrary/src/Tests/Tests.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include <string>
+#include <chrono>
 
 class Tests
 {
@@ -9,10 +10,17 @@ public:
 private:
 
 	void ShowIntroduction();
-	void MainOptions(bool&);
+	bool MainOptions();
 	void TypeCategory();
 
-	typedef enum {
+	void StartTimer();
+	void StopTimer();
+	int nanosecondsDifference();
+
+	std::chrono::steady_clock::time_point _start;
+	std::chrono::steady_clock::time_point _stop;
+
+	enum class MAIN_TEST_TYPES {
 		STATIC,
 		BASIC,
 		TRIG,
@@ -22,8 +30,10 @@ private:
 		ENCODING,
 		LERPING,
 		RANDOMNESS
-	} MAIN_TEST_TYPES;
-	std::map<int, std::string> allMainOptionsText = std::map<int, std::string>{
+	} ;
+
+	std::map<MAIN_TEST_TYPES, std::string> allMainOptionsText = std::map<MAIN_TEST_TYPES, std::string>{
+		
 		{MAIN_TEST_TYPES::STATIC,"STATIC TESTS"},
 		{MAIN_TEST_TYPES::BASIC,"BASIC TESTS"},
 		{MAIN_TEST_TYPES::TRIG,"TRIG TESTS"},
@@ -36,246 +46,243 @@ private:
 	};
 
 	//STATIC
-		typedef enum {
-			DEG_2_RAD,
-			INFINITY__DEFINED,
-			NEGATIVE_INFINITY,
-			PI__DEFINED,
-			RAD_2_DEGREE
+	enum class STATIC_TESTS {
+		DEG_2_RAD,
+		INFINITY__DEFINED,
+		NEGATIVE_INFINITY,
+		PI__DEFINED,
+		RAD_2_DEGREE
+	} ;
 
-		} STATIC_TESTS;
-
-		std::map<int, std::string> allStaticOptionsText = std::map<int, std::string>{
+	std::map<STATIC_TESTS, std::string> allStaticOptionsText = std::map<STATIC_TESTS, std::string>{
 		{STATIC_TESTS::DEG_2_RAD , "Degrees to Radians"},
-			{STATIC_TESTS::INFINITY__DEFINED,"Infinity"},
-			{STATIC_TESTS::NEGATIVE_INFINITY,"Negative Infinity"},
-			{STATIC_TESTS::PI__DEFINED,"PI"},
-			{STATIC_TESTS::RAD_2_DEGREE,"Radians to Degrees"}
-		};
-		void DegToRad();
-		void Infinity();
-		void NegativeInfinity();
-		void PI();
-		void Rad2Deg();
+		{STATIC_TESTS::INFINITY__DEFINED,"Infinity"},
+		{STATIC_TESTS::NEGATIVE_INFINITY,"Negative Infinity"},
+		{STATIC_TESTS::PI__DEFINED,"PI"},
+		{STATIC_TESTS::RAD_2_DEGREE,"Radians to Degrees"}
+	};
+	void DegToRad();
+	void Infinity();
+	void NegativeInfinity();
+	void PI();
+	void Rad2Deg();
 
-		void StaticTests();
+	void StaticTests();
 
 	//BASIC
-		typedef enum {
-			ABS,
-			APPROX,
-			CIEL,
-			CIEL_TO_INT,
-			CLAMP,
-			CLAMP01,
-			FLOOR,
-			FLOOR_TO_INT,
-			MAX,
-			MIN,
-			POW,
-			ROUND,
-			ROUND_TO_INT,
-			SIGN,
-			SQRT
-		} BASIC_TESTS;
+	enum class BASIC_TESTS  {
+		ABS,
+		APPROX,
+		CIEL,
+		CIEL_TO_INT,
+		CLAMP,
+		CLAMP01,
+		FLOOR,
+		FLOOR_TO_INT,
+		MAX,
+		MIN,
+		POW,
+		ROUND,
+		ROUND_TO_INT,
+		SIGN,
+		SQRT
+	} ;
 
-		std::map<int, std::string> allBasicOptionsText = std::map<int, std::string>{
-			{BASIC_TESTS::ABS, "Abs"},
-			{BASIC_TESTS::APPROX, "Approx"},
-			{BASIC_TESTS::CIEL,"Ciel"},
-			{BASIC_TESTS::CIEL_TO_INT,"Cieling to Int"},
-			{BASIC_TESTS::CLAMP,"Clamp"},
-			{BASIC_TESTS::CLAMP01,"Clamp01"},
-			{BASIC_TESTS::FLOOR, "Floor"},
-			{BASIC_TESTS::FLOOR_TO_INT,"Floor to Int"},
-			{BASIC_TESTS::MAX,"Max"},
-			{BASIC_TESTS::MIN,"Min"},
-			{BASIC_TESTS::POW,"Power"},
-			{BASIC_TESTS::ROUND,"Round"},
-			{BASIC_TESTS::ROUND_TO_INT,"Round to Int"},
-			{BASIC_TESTS::SIGN,"Sign"},
-			{BASIC_TESTS::SQRT,"Square Root"}
-		};
+	std::map<BASIC_TESTS, std::string> allBasicOptionsText = std::map<BASIC_TESTS, std::string>
+	{
+		{BASIC_TESTS::ABS, "Abs"},
+		{BASIC_TESTS::APPROX, "Approx"},
+		{BASIC_TESTS::CIEL,"Ciel"},
+		{BASIC_TESTS::CIEL_TO_INT,"Cieling to Int"},
+		{BASIC_TESTS::CLAMP,"Clamp"},
+		{BASIC_TESTS::CLAMP01,"Clamp01"},
+		{BASIC_TESTS::FLOOR, "Floor"},
+		{BASIC_TESTS::FLOOR_TO_INT,"Floor to Int"},
+		{BASIC_TESTS::MAX,"Max"},
+		{BASIC_TESTS::MIN,"Min"},
+		{BASIC_TESTS::POW,"Power"},
+		{BASIC_TESTS::ROUND,"Round"},
+		{BASIC_TESTS::ROUND_TO_INT,"Round to Int"},
+		{BASIC_TESTS::SIGN,"Sign"},
+		{BASIC_TESTS::SQRT,"Square Root"}
+	};
 
-		void Abs();
-		void Approx();
-		void Ciel();
-		void CielToInt();
-		void Clamp();
-		void Clamp01();
-		void Floor();
-		void FloorToInt();
-		void Max();
-		void Min();
-		void Pow();
-		void Round();
-		void RoundToInt();
-		void Sign();
-		void Sqrt();
+	void Abs();
+	void Approx();
+	void Ciel();
+	void CielToInt();
+	void Clamp();
+	void Clamp01();
+	void Floor();
+	void FloorToInt();
+	void Max();
+	void Min();
+	void Pow();
+	void Round();
+	void RoundToInt();
+	void Sign();
+	void Sqrt();
 
-		void BasicTests();
+	void BasicTests();
 
-	////TRIG:
-		typedef enum {
-			ACOS,
-			ASIN,
-			ATAN,
-			ATAN2,
-			COS,
-			DELTAANGLE,
-			SIN,
-			TAN
-		} TRIG_TESTS;
+	//TRIG:
+	enum class TRIG_TESTS {
+		ACOS,
+		ASIN,
+		ATAN,
+		ATAN2,
+		COS,
+		DELTAANGLE,
+		SIN,
+		TAN
+	};
 
-		std::map<int, std::string> allTrigOptionsText = std::map<int, std::string>{
-			{TRIG_TESTS::ACOS, "ACos"},
-			{TRIG_TESTS::ASIN,"Asin"},
-			{TRIG_TESTS::ATAN,"Atan"},
-			{TRIG_TESTS::ATAN2,"Atan2"},
-			{TRIG_TESTS::COS,"Cos"},
-			{TRIG_TESTS::DELTAANGLE,"DeltaAngle"},
-			{TRIG_TESTS::SIN,"Sine"},
-			{TRIG_TESTS::TAN,"Tan"}
-		};
+	std::map<TRIG_TESTS, std::string> allTrigOptionsText = std::map<TRIG_TESTS, std::string>{
+		{TRIG_TESTS::ACOS, "ACos"},
+		{TRIG_TESTS::ASIN,"Asin"},
+		{TRIG_TESTS::ATAN,"Atan"},
+		{TRIG_TESTS::ATAN2,"Atan2"},
+		{TRIG_TESTS::COS,"Cos"},
+		{TRIG_TESTS::DELTAANGLE,"DeltaAngle"},
+		{TRIG_TESTS::SIN,"Sine"},
+		{TRIG_TESTS::TAN,"Tan"}
+	};
 
-		void Acos();
-		void Asin();
-		void Atan();
-		void Atan2();
-		void Cos();
-		void DeltaAngle();
-		void Sin();
-		void Tan();
+	void Acos();
+	void Asin();
+	void Atan();
+	void Atan2();
+	void Cos();
+	void DeltaAngle();
+	void Sin();
+	void Tan();
 
-		void TrigTests();
+	void TrigTests();
 
-	////POWERS
+	//POWERS
 
-		typedef enum {
-			CLOSEST_POWER_OF_TWO,
-			IS_POWER_OF_TWO,
-			NEXT_POWER_OF_TWO
-		} POWERS_TESTS;
+	enum class POWERS_TESTS{
+		CLOSEST_POWER_OF_TWO,
+		IS_POWER_OF_TWO,
+		NEXT_POWER_OF_TWO
+	} ;
 
-		std::map<int, std::string> allPowersOptionsText = std::map<int, std::string>{
-			{POWERS_TESTS::CLOSEST_POWER_OF_TWO,"Closest Power of Two"},
-			{POWERS_TESTS::IS_POWER_OF_TWO, "Is Power of Two"},
-			{POWERS_TESTS::NEXT_POWER_OF_TWO, "Next Power of Two"}
-		};
+	std::map<POWERS_TESTS, std::string> allPowersOptionsText = std::map<POWERS_TESTS, std::string>{
+		{POWERS_TESTS::CLOSEST_POWER_OF_TWO,"Closest Power of Two"},
+		{POWERS_TESTS::IS_POWER_OF_TWO, "Is Power of Two"},
+		{POWERS_TESTS::NEXT_POWER_OF_TWO, "Next Power of Two"}
+	};
 
-		void ClosestPowerOfTwo();
-		void IsPowerOfTwo();
-		void NextPowerOfTwo();
+	void ClosestPowerOfTwo();
+	void IsPowerOfTwo();
+	void NextPowerOfTwo();
 
-		void PowersTests();
+	void PowersTests();
 
-	////EXPONENTIALS
-		typedef enum {
-			EXP,
-			LOG,
-			LOG10
-		} EXPONENTIALS_TESTS;
+	//EXPONENTIALS
+	enum class EXPONENTIALS_TESTS  {
+		EXP,
+		LOG,
+		LOG10
+	} ;
 
-		std::map<int, std::string> allExponentialsOptionsText = std::map<int, std::string>{
-			{EXPONENTIALS_TESTS::EXP,"Exponential"},
-			{EXPONENTIALS_TESTS::LOG, "Ln"},
-			{EXPONENTIALS_TESTS::LOG10, "Log 10"}
-		};
+	std::map<EXPONENTIALS_TESTS, std::string> allExponentialsOptionsText = std::map<EXPONENTIALS_TESTS, std::string>{
+		{EXPONENTIALS_TESTS::EXP,"Exponential"},
+		{EXPONENTIALS_TESTS::LOG, "Ln"},
+		{EXPONENTIALS_TESTS::LOG10, "Log 10"}
+	};
 
-		void Exp();
-		void Log();
-		void Log10();
+	void Exp();
+	void Log();
+	void Log10();
 
-		void ExponentialsTests();
+	void ExponentialsTests();
 
-	////COLOUR
-		typedef enum {
-			GAMMA_TO_LINEAR,
-			LINEAR_TO_GAMMA
-		} COLOUR_TESTS;
+	//COLOUR
+	enum class COLOUR_TESTS {
+		GAMMA_TO_LINEAR,
+		LINEAR_TO_GAMMA
+	} ;
 
-		std::map<int, std::string> allColourOptionsText = std::map<int, std::string>{
-			{COLOUR_TESTS::GAMMA_TO_LINEAR,"Gamma To Linear"},
-			{COLOUR_TESTS::LINEAR_TO_GAMMA,"Linear to Gamma"}
-		};
+	std::map<COLOUR_TESTS, std::string> allColourOptionsText = std::map<COLOUR_TESTS, std::string>{
+		{COLOUR_TESTS::GAMMA_TO_LINEAR,"Gamma To Linear"},
+		{COLOUR_TESTS::LINEAR_TO_GAMMA,"Linear to Gamma"}
+	};
 
-		void GammaToLinear();
-		void LinearToGamma();
+	void GammaToLinear();
+	void LinearToGamma();
 
-		void ColourTests();
+	void ColourTests();
 
-	////ENCODING
-		typedef enum {
-			FLOAT_TO_HALF,
-			HALF_TO_FLOAT
-		} ENCODING_TESTS;
+	//ENCODING
+	enum class ENCODING_TESTS {
+		FLOAT_TO_HALF,
+		HALF_TO_FLOAT
+	} ;
 
-		std::map<int, std::string> allEncodingOptionsText = std::map<int, std::string>{
-			{ENCODING_TESTS::FLOAT_TO_HALF, "Float to Half"},
-			{ENCODING_TESTS::HALF_TO_FLOAT, "Half to Float"}
+	std::map<ENCODING_TESTS, std::string> allEncodingOptionsText = std::map<ENCODING_TESTS, std::string>{
+		{ENCODING_TESTS::FLOAT_TO_HALF, "Float to Half"},
+		{ENCODING_TESTS::HALF_TO_FLOAT, "Half to Float"}
 
-		};
+	};
 
-		void FloatToHalf();
-		void HalfToFloat();
+	void FloatToHalf();
+	void HalfToFloat();
 
-		void EncodingTests();
+	void EncodingTests();
 
-	////LERPING
-		typedef enum {
-			INVERSE_LERP,
-			LERP,
-			LERP_ANGLE,
-			LERP_UNCLAMPED,
-			MOVE_TOWARDS,
-			MOVE_TOWARDS_ANGLE,
-			SMOOTH_DAMP,
-			SMOOTH_DAMP_ANGLE,
-			SMOOTH_STEP
-		} LERPING_TESTS;
+	//LERPING
+	enum class LERPING_TESTS {
+		INVERSE_LERP,
+		LERP,
+		LERP_ANGLE,
+		LERP_UNCLAMPED,
+		MOVE_TOWARDS,
+		MOVE_TOWARDS_ANGLE,
+		SMOOTH_DAMP,
+		SMOOTH_DAMP_ANGLE,
+		SMOOTH_STEP
+	};
 
-		std::map<int, std::string> allLerpingOptionsText = std::map<int, std::string>{
-			{LERPING_TESTS::INVERSE_LERP, "Inverse Lerp"},
-			{LERPING_TESTS::LERP, "Lerp"},
-			{LERPING_TESTS::LERP_ANGLE, "Lerp Angle"},
-			{LERPING_TESTS::LERP_UNCLAMPED, "Lerp Unclamped"},
-			{LERPING_TESTS::MOVE_TOWARDS, "Move Towards"},
-			{LERPING_TESTS::MOVE_TOWARDS_ANGLE,"Move Towards Angle"},
-			{LERPING_TESTS::SMOOTH_DAMP, "Smooth Damp"},
-			{LERPING_TESTS::SMOOTH_DAMP_ANGLE, "Smooth Damp Angle"},
-			{LERPING_TESTS::SMOOTH_STEP, "Smooth Step"}
-		};
+	std::map<LERPING_TESTS, std::string> allLerpingOptionsText = std::map<LERPING_TESTS, std::string>{
+		{LERPING_TESTS::INVERSE_LERP, "Inverse Lerp"},
+		{LERPING_TESTS::LERP, "Lerp"},
+		{LERPING_TESTS::LERP_ANGLE, "Lerp Angle"},
+		{LERPING_TESTS::LERP_UNCLAMPED, "Lerp Unclamped"},
+		{LERPING_TESTS::MOVE_TOWARDS, "Move Towards"},
+		{LERPING_TESTS::MOVE_TOWARDS_ANGLE,"Move Towards Angle"},
+		{LERPING_TESTS::SMOOTH_DAMP, "Smooth Damp"},
+		{LERPING_TESTS::SMOOTH_DAMP_ANGLE, "Smooth Damp Angle"},
+		{LERPING_TESTS::SMOOTH_STEP, "Smooth Step"}
+	};
 
-		void InverseLerp();
-		void Lerp();
-		void LerpAngle();
-		void LerpUnclamped();
-		void MoveTowards();
-		void MoveTowardsAngle();
-		void SmoothDamp();
-		void SmoothDampAngle();
-		void SmoothStep();
+	void InverseLerp();
+	void Lerp();
+	void LerpAngle();
+	void LerpUnclamped();
+	void MoveTowards();
+	void MoveTowardsAngle();
+	void SmoothDamp();
+	void SmoothDampAngle();
+	void SmoothStep();
 
-		void LerpingTests();
+	void LerpingTests();
 
-	////RANDOMNESS
-		typedef enum {
-			PERLIN_NOISE,
-			PING_PONG,
-			REPEAT
-		} RANDOMNESS_TESTS;
-		std::map<int, std::string> allRandomnessOptionsText = std::map<int, std::string>{
-			{RANDOMNESS_TESTS::PERLIN_NOISE,"Perlin Noise"},
-			{RANDOMNESS_TESTS::PING_PONG, "Ping Pong"},
-			{RANDOMNESS_TESTS::REPEAT,"Repeat"}
-		};
+	//RANDOMNESS
+	enum class RANDOMNESS_TESTS {
+		PERLIN_NOISE,
+		PING_PONG,
+		REPEAT
+	} ;
+	std::map<RANDOMNESS_TESTS, std::string> allRandomnessOptionsText = std::map<RANDOMNESS_TESTS, std::string>{
+		{RANDOMNESS_TESTS::PERLIN_NOISE,"Perlin Noise"},
+		{RANDOMNESS_TESTS::PING_PONG, "Ping Pong"},
+		{RANDOMNESS_TESTS::REPEAT,"Repeat"}
+	};
 
-		void PerlinNoise();
-		void PingPong();
-		void Repeat();
+	void PerlinNoise();
+	void PingPong();
+	void Repeat();
 
-		void RandomnessTests();
-
-
+	void RandomnessTests();
 };
-
