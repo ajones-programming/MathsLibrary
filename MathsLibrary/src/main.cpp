5b35544aa2f38ba02b1fde182a48ff7d1759a687
ajones-programming <ajones.programming@gmail.com>
2024-06-03 22:22:40 +0100

    got rid of random comments!

--- MathsLibrary/src/main.cpp
+++ MathsLibrary/src/main.cpp
@@ -5,16 +5,5 @@
 
 int main() {
 	Tests tests = Tests();
-	//std::cout << "HEWWO" << std::endl;
-	//float value;
-	//std::cout << "ENTER VALUE TO TURN FROM DEGREES TO RADIANS:";
-	//std::cin >> value;
-	//std::cout << std::endl;
-	//std::cout << "THE RESULT IS " << std::to_string(MathsLibrary::Deg2Rad(value)) << std::endl;
-
-	//std::cout << "THE SIZE OF INFINITY IS " << std::to_string( MathsLibrary::Infinity) << std::endl;
-	//std::cout << "THE SIZE OF NEGATIVE INFINITY IS " << std::to_string( MathsLibrary::NegativeInfinity) << std::endl;
-	//std::cout << "THE SIZE OF PI IS " << std::to_string( MathsLibrary::PI) << std::endl;
-	//std::cout << "BACK TO DEGREES IS " << std::to_string(MathsLibrary::Rad2Deg(MathsLibrary::Deg2Rad(value))) << std::endl;
 	return 0;
 }
